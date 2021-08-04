4d3c59ef2c1930310601500910b6138d83bb2d1e
aqib <hmaqib91@gmail.com>
2021-08-04 10:10:17 +0500

    Save to Github

--- lib/Models/imagesMaodel.dart
+++
@@ -1,8 +0,0 @@
-// class ImagesModel {
-//   //var photographer;
-//   var url;
-//   // ignore: non_constant_identifier_names
-//   //var photographer_id;
-//   // ignore: non_constant_identifier_names
-//   ImagesModel({required this.url});
-// }
